bd537ee5a2ade2d61aa9018d255f2fd9e56ec31f
qi xu <qi.xu@carrier.com>
2022-11-04 12:23:37 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,84 @@ class HomeApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.cyan[400],
         ),
-        body: const Center(
-          child: Text(
-            'Hello Qi !',
-            style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),
-          ),
+        //layout
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R1'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R1'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R1'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R1'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R1'),
+                  ),
+                ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.red[400],
+                    child: const Text('R2'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R2'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R2'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R2'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.cyan,
+                    child: const Text('R2'),
+                  ),
+                ]),
+          ],
         ),
+
+        // body: Center(
+
+        // child: Icon(Icons.adb_sharp, color: Colors.green[800], size: 100.0),
+
+        // image: NetworkImage(
+        //     'https://images.pexels.com/photos/13046530/pexels-photo-13046530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+        //     ),
+        // child: Text(
+        //   'Hello Qi !',
+        //   style: TextStyle(
+        //       fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),
+        // ),
+        // ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             print('You clicked me');
